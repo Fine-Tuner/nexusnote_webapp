7264341f6a31998bf5fd57cb542e6fef2dc99259
TaejoonPark <joker77z@naver.com>
2025-02-06 21:19:32 +0900

    feat: Add shadcn/ui Button component and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import UpdateElectron from "@/components/update";
 import logoVite from "./assets/logo-vite.svg";
 import logoElectron from "./assets/logo-electron.svg";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
+      <Button variant={"destructive"}>Button</Button>
       <UpdateElectron />
     </div>
   );
